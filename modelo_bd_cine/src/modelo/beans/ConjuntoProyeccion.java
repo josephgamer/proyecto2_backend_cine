37c04ba7cab4a40676c94a2c0cd881503e3e4056
Esteban <jose.gonzalez.h@est.una.ac.cr>
2021-06-18 11:24:10 -0600

    Popup menu habilitado

--- modelo_bd_cine/src/modelo/beans/ConjuntoProyeccion.java
+++ modelo_bd_cine/src/modelo/beans/ConjuntoProyeccion.java
@@ -79,6 +79,8 @@ public class ConjuntoProyeccion implements Serializable{
             r.append(String.format("<p>Día: %s</p>", item.getHorario().getDia()));
             r.append(String.format("<p>Precio: %d</p>", item.getSala().getPrecio()));
             r.append(String.format("<p>Tipo Sala: %s</p>", item.getSala().getTipo()));
+            r.append(String.format("<p><button class = '%s' type='%s' onclick='%s'>%s</button></p>",
+                    "btn","button","mostrar(2);","Butacas"));
             
             r.append("</td>");
             k++;
